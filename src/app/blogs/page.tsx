7ec7a82caf11cc2a4dc9b64fdc71c2012c2064e9
omprakash987 <kanuparewa371@gmail.com>
2024-06-20 01:41:48 +0530

    blog page edit

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -4,52 +4,49 @@
 import React from 'react'
 import { Appbar } from '@/components/Appbar'
 import { BlogCard } from '@/components/BlogCard'
- 
 import { useBlogs } from '../../hooks/useBlogs'
 import { BlogSkeleton } from '@/components/skeleton'
 
+const Blogs = () => {
+   const { loading, blogs } = useBlogs(); 
 
-export const Blogs = () => {
-   const{loading,blogs} = useBlogs(); 
-
-
-   if(loading){
-    return(
-      <div>
-        <Appbar/>
-        <div className=' flex justify-center'>
-          <div>
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-          </div>
-
-        </div>
-      </div>
-    )
+   if (loading) {
+      return (
+         <div>
+            <Appbar/>
+            <div className=' flex justify-center'>
+               <div>
+                  <BlogSkeleton />
+                  <BlogSkeleton />
+                  <BlogSkeleton />
+                  <BlogSkeleton />
+                  <BlogSkeleton />
+               </div>
+            </div>
+         </div>
+      );
    }
 
-  return (
-    <div>
-      <Appbar/>
-      <div className=' flex justify-center'>
-        <div>
-        {blogs.map(blog => <BlogCard
-        
-                    key={blog.id}
-                    id={blog.id}
-                    authorName={blog.author.name || "Anonymous"}
-                    title={blog.title}
-                    content={blog.content}
-                    publishedDate={"2nd Feb 2024"}
-                />)}
-        </div>
-      </div>
+   return (
+      <div>
+         <Appbar/>
+         <div className=' flex justify-center'>
+            <div>
+               {blogs.map(blog => (
+                  <BlogCard
 
-    </div>
-  )
+                     key={blog.id}
+                     id={blog.id}
+                     authorName={blog.author.name || "Anonymous"}
+                     title={blog.title}
+                     content={blog.content}
+                     publishedDate={"2nd Feb 2024"}
+                  />
+               ))}
+            </div>
+         </div>
+      </div>
+   );
 }
 
-export default Blogs
+export default Blogs;
