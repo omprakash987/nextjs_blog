0c6d2ec96b17d4606dd9a89cbacb42f75e82766f
omprakash987 <kanuparewa371@gmail.com>
2024-06-20 21:16:30 +0530

    cors error

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,20 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+
+
+
+
+
+module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://api.example.com/:path*',
+          },
+        ]
+      },
+  };
