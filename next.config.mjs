c7a65e20f867c09306b9eba212be864964d4849d
omprakash987 <kanuparewa371@gmail.com>
2024-06-20 21:19:32 +0530

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://api.example.com/:path*',
+          },
+        ]
+      },
+};
 
 export default nextConfig;
 
@@ -9,13 +18,3 @@ export default nextConfig;
 
 
 
-module.exports = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://api.example.com/:path*',
-          },
-        ]
-      },
-  };
