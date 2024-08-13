7eec3fb9ed3c4e54b7a8a70791994013ae2a2d47
omprakash987 <kanuparewa371@gmail.com>
2024-08-13 21:01:16 +0530

    next-config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://api.example.com/:path*',
-            },
-        ];
-    },
-    async headers() {
-        return [
-            {
-                source: '/api/:path*', // Matches all API routes
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*', // Allow all origins, change this to your specific origin as needed
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'Content-Type, Authorization',
-                    },
-                ],
-            },
-        ];
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
